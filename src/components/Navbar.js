b3a8713e7724bacbe6f537a55412206b9d8cad3c
Manux21 <Manux21@yandex.ru>
2022-08-31 23:31:39 +0300

    Added Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const Navbar = () => (
+  <nav className="navbar navbar-dark bg-primary navbar-expand-lg" style={{paddingLeft: '20px'}}>
+    <div className="navbar-brand">
+      Github Поиск
+    </div>
+
+    <ul className="navbar-nav">
+      <li className='nav-item'>
+        <a href="#" className='nav-link'>Главная</a>
+      </li>
+
+      <li className='nav-item'>
+        <a href="#" className='nav-link'>Информация</a>
+      </li>
+    </ul>
+  </nav>
+)
