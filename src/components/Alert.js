8eb1f246b0c0158f2f80eeb72779887252568fe9
Manux21 <Manux21@yandex.ru>
2022-09-02 00:21:51 +0300

    Updated Alert

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {AlertContext} from "../context/alert/alertContext";
 
-const Alert = ({alert}) => {
+const Alert = () => {
+  const {alert, hide} = useContext(AlertContext)
+
+  if (!alert) return null
+
   return (
     <div
       className={`alert alert-${alert.type || 'secondary'} alert-dismissible`}
       role="alert"
     >
       {alert.text}
-      <button type="button" className="btn-close" aria-label="Close"></button>
+      <button type="button" className="btn-close" aria-label="Close" onClick={hide}></button>
     </div>
   );
 };
