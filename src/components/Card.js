ec3da4160dc7e3ab2d80aaea9f5688a4467354a0
Manux21 <Manux21@yandex.ru>
2022-09-07 01:31:22 +0300

    Updated

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-export const Card = () => (
+export const Card = ({user}) => (
   <div className='card'>
-    <img className='card-image-t' src={''} alt={''}/>
+    <img className='card-image-t' src={user.avatar_url} alt={user.login}/>
     <div className="card-body">
-      <h5 className='card-title'>React JS</h5>
-      <Link to={'/profile/react'} className='btn btn-primary'>Открыть</Link>
+      <h5 className='card-title'>user.login</h5>
+      <Link to={'/profile/' + user.login} className='btn btn-primary'>Открыть</Link>
     </div>
   </div>
 )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,25 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {Search} from "../components/Search";
 import {Card} from "../components/Card";
+import {GithubContext} from "../context/github/githubContext";
 
 export const Home = () => {
 
+  const {loading,users} = useContext(GithubContext)
 
-  const cards = new Array(15)
-    .fill('')
-    .map((_, i) => i)
   return (
     <>
       <Search/>
       <div className="row" style={{marginTop: '10px'}}>
-        {cards.map(card => {
-          return(
-            <div className="col-sm-4 mb-4" key={card}>
-              <Card/>
+
+        {loading
+          ? <p className={'text-center'}>Загрузка...</p>
+          : users.map(user=> (
+            <div className="col-sm-4 mb-4" key={user.id}>
+              <Card user={user}/>
             </div>
           )
-        })}
+          )}
       </div>
     </>
   )
